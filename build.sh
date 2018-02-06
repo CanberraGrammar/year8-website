59b1fb586a8df4f2f5ebb28afd761cdf648123e3
Matthew Purcell <matthew.purcell@cgs.act.edu.au>
2018-02-07 08:09:00 +1100

    Year 8 Updates

--- build.sh
+++ build.sh
@@ -9,31 +9,31 @@ fi
 # enable error reporting to the console
 set -e
 
-# currently in /home/travis/build/CanberraGrammar/year9-website
+# currently in /home/travis/build/CanberraGrammar/year8-website
 
 # build site with jekyll, by default to `_site' folder
 bundle exec jekyll build
 
 # cleanup
-rm -rf ../year9.cgscomputing.com
+rm -rf ../year8.cgscomputing.com
 
-# clone repository into /home/travis/build/CanberraGrammar/year9.cgscomputing.com
-git clone https://${GH_TOKEN}@github.com/CanberraGrammar/year9-website.git ../year9.cgscomputing.com
+# clone repository into /home/travis/build/CanberraGrammar/year8.cgscomputing.com
+git clone https://${GH_TOKEN}@github.com/CanberraGrammar/year8-website.git ../year8.cgscomputing.com
 
 # change to the gh-pages branch
-cd ../year9.cgscomputing.com
+cd ../year8.cgscomputing.com
 git checkout gh-pages
-cd ../year9-website
+cd ../year8-website
 
 # delete all files in cloned copy (in case this commit has deleted files)
-rm -rf ../year9.cgscomputing.com/*
+rm -rf ../year8.cgscomputing.com/*
 
 # copy generated HTML site to `gh-pages' branch
-cp -rf _site/* ../year9.cgscomputing.com
+cp -rf _site/* ../year8.cgscomputing.com
 
 # commit and push generated content to `gh-pages' branch
 # since repository was cloned in write mode with token auth - we can push there
-cd ../year9.cgscomputing.com
+cd ../year8.cgscomputing.com
 git config user.email "cgscomputing@cgs.act.edu.au"
 git config user.name "CGSComputing"
 git add -A .
